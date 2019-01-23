db80a32ffba55fe350fad3be9d213d19b95acb74
JeevanandhamPS <psjeeva97@gmail.com>
2019-01-23 15:34:22 +0530

    posix shared memory

---
+++ posix_shared_memory/send.c
@@ -0,0 +1,57 @@
+
+/*###################*\
+~   SCHOOL OF LINUX   ~ 
+\*###################*/
+
+
+
+ /**************************************\
+  *                                    * 
+  *  Author      : Jeevanandham.P.S    *
+  *  Date        : 23-01-2019          *
+  *  Filename    : send.c              *
+  *  Description : Explains            *
+  *                POSIX shared memory *
+  *                                    *
+ \**************************************/
+
+
+
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <error.h>
+#include <stdio.h>
+
+unsigned char str[] = "POSIX shared memory as working sucessfully";
+
+int main()
+{
+	int shmfd,sync,unmap,size,ulin;
+	unsigned char *vir_addr;
+	ssize_t wrt;
+
+	size = strlen(str);
+
+	//Create and open a POSIX shared memory..
+	shmfd = shm_open("/shared_posix", O_CREAT | O_RDWR, 0644);//shm_open returns a descriptor to shmfd..
+	perror("shm_open");
+
+	wrt = write(shmfd,str,size);//write a string..
+
+	//we need an virtual address to share a memory..
+	vir_addr = mmap(NULL,size,PROT_READ | PROT_WRITE,MAP_SHARED,shmfd,0);//mmap always return virtual address of the file descriptor(4th argument)..
+	perror("mmap");
+
+	//synchronize a file with a memory map..
+	sync = msync(vir_addr,size,MS_SYNC);//virtual address as mapped..
+	perror("msync");
+
+	//unmapping..
+	unmap = munmap(vir_addr,10);
+	perror("munmap");
+	return 0;
+}
