230597c5261768a7222dd421c9009b7b53da053e
JeevanandhamPS <psjeeva97@gmail.com>
2019-01-24 12:24:49 +0530

    posix shared memory

---
+++ posix_shm/posix_samp/shmreceive.h
@@ -0,0 +1,22 @@
+/***************************************/
+/* Author	: Jeevanandham.P.S     */
+/* Date		: 23jan2019            */
+/* Filename	: pxshm.h              */
+/* Description	: header files         */
+/***************************************/
+
+#ifndef SHMRECEIVE_H
+#define SHMRECEIVE_H
+
+
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <error.h>
+#include <stdio.h>
+
+
+#endif //SHMRECEIVE_H
---
+++ posix_shm/posix_samp/shmsend.h
@@ -0,0 +1,22 @@
+/***************************************/
+/* Author	: Jeevanandham.P.S     */
+/* Date		: 23jan2019            */
+/* Filename	: pxshm.h              */
+/* Description	: header files         */
+/***************************************/
+
+#ifndef SHMSEND_H
+#define SHMSEND_H
+
+
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <error.h>
+#include <stdio.h>
+
+
+#endif //SHMSEND_H
