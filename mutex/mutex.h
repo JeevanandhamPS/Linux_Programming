3d170a969c5f9afaa392b56b3fbb95de442ddf25
JeevanandhamPS <psjeeva97@gmail.com>
2019-02-06 22:35:00 +0530

    multithread using mutex

---
+++ mutex/mutex.h
@@ -0,0 +1,24 @@
+/***************************************/
+/* Author	: Jeevanandham.P.S     */
+/* Date		: 6-feb-2019           */
+/* Filename	: mutex.h              */
+/* Description	: header files         */
+/***************************************/
+
+#ifndef MUTEX_H
+#define MUTEX_H
+
+
+/* Included header files */
+
+#include <stdio.h>
+#include <error.h>
+#include <pthread.h>
+
+/* Macro Definitions */
+
+void *thread_fun(void *);
+
+/* user defined datatype definitions */
+
+#endif //MUTEX
