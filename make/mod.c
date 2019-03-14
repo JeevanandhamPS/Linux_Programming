98cb98aa86b731a509f0034fa714a5c5e3001a30
JeevanandhamPS <psjeeva97@gmail.com>
2019-03-14 13:43:04 +0530

    Sample for make

---
+++ make/mod.c
@@ -0,0 +1,19 @@
+/***************************************/
+/* Author	: Jeevanandham P S     */
+/* Date		: 14-mar-2019          */
+/* Filename	: mod.c                */
+/* Description	: sample make file     */
+/***************************************/
+
+/* Included header file */
+#include "make.h"
+
+/* Global variable definitions */
+
+
+/* function definitions */
+
+int mod(int x,int y)
+{
+	return (x%y);
+}
---
+++ make/mul.c
@@ -0,0 +1,19 @@
+/***************************************/
+/* Author	: Jeevanandham P S     */
+/* Date		: 14-mar-2019          */
+/* Filename	: mul.c                */
+/* Description	: sample make file     */
+/***************************************/
+
+/* Included header file */
+#include "make.h"
+
+/* Global variable definitions */
+
+
+/* function definitions */
+
+int mul(int x,int y)
+{
+	return (x*y);
+}
