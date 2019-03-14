30b89e097f3eadd801a8d8ba72220fdc8df7957d
JeevanandhamPS <psjeeva97@gmail.com>
2019-03-14 18:58:42 +0530

    sample for make

---
+++ make/sample/div.c
@@ -0,0 +1,19 @@
+/***************************************/
+/* Author	: Jeevanandham P S     */
+/* Date		: 14-mar-2019          */
+/* Filename	: div.c                */
+/* Description	: sample make file     */
+/***************************************/
+
+/* Included header file */
+#include "make.h"
+
+/* Global variable definitions */
+
+
+/* function definitions */
+
+int div(int x,int y)
+{
+	return (x/y);
+}
