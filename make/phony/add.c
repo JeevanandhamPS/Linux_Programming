769518d86270fc7bf10cf6c18f371a6ae817f483
JeevanandhamPS <psjeeva97@gmail.com>
2019-03-14 18:39:40 +0530

    Makefile with phony target

---
+++ make/phony/add.c
@@ -0,0 +1,19 @@
+/***************************************/
+/* Author	: Jeevanandham P S     */
+/* Date		: 14-mar-2019          */
+/* Filename	: add.c                */
+/* Description	: sample make file     */
+/***************************************/
+
+/* Included header file */
+#include "make.h"
+
+/* Global variable definitions */
+
+
+/* function definitions */
+
+int add(int x,int y)
+{
+	return (x+y);
+}
