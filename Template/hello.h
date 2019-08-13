2f9ed3346fc2b43c06adb9f29e33ec6ef1d6b7f4
P . S . JEEVA11 <42694368+JeevanandhamPS@users.noreply.github.com>
2019-08-13 15:10:02 +0530

    header file

--- Template/hello.h
+++ Template/hello.h
@@ -4,3 +4,23 @@
 /* Filename	: hello.h         */
 /* Description	: Header file    */
 /***************************************/
+
+#ifndef HELLO_H
+#define HELLO_H
+
+
+/* Included header files */
+#include<stdio.h>
+
+
+/* Macro Definitions */
+
+
+/* user defined datatype definitions */
+
+
+/* function prototypes */
+
+
+
+#endif /* HELLO_H */
